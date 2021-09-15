434bd057c219e24dcebab257f97e59557c289453
an.xi <an.xi@amlogic.com>
2021-09-15 15:51:33 +0800

    cec: only register tvinput listener on tv [1/1]
    PD#SWPL-59449
    
    Problem:
    The TvInput app maybe lose permission HDMI_CEC
    somehow when replaced with GSI system image.
    
    Solution:
    Only register tvinput listener on tv.
    
    Verify:
    ohm
    
    Change-Id: I94e475a0b0712d5200b2b2b385965405b486f2dc
    Signed-off-by: an.xi <an.xi@amlogic.com>

--- DroidLogicTvInput/src/com/droidlogic/tvinput/BootReceiver.java
+++ DroidLogicTvInput/src/com/droidlogic/tvinput/BootReceiver.java
@@ -9,6 +9,7 @@
 
 package com.droidlogic.tvinput;
 
+import android.os.SystemProperties;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -20,10 +21,15 @@ import com.droidlogic.app.tv.InputChangeAdapter;
 public class BootReceiver extends BroadcastReceiver {
     private static final String TAG = "BootReceiver";
 
+    private static final String PROP_IS_TV = "ro.vendor.platform.has.tvuimode";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d(TAG,"BootCompelte start otp if possbile");
-        InputChangeAdapter.getInstance(context).sendBootOtpIntent();
+        if (SystemProperties.getBoolean(PROP_IS_TV, false)) {
+            Log.d(TAG,"BootCompelte start otp if possbile");
+            InputChangeAdapter.getInstance(context).sendBootOtpIntent();
+        }
+        Log.d(TAG, "onReceive boot complete broadcast");
     }
 }
 
